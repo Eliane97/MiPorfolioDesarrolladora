763f0df472d366d31424b4a6b15cf99cb132aa1b
Eliane <101228113+Eliane97@users.noreply.github.com>
2025-12-09 21:25:39 -0300

    Update accion.js
    a ver

--- accion.js
+++ accion.js
@@ -1,49 +1,35 @@
-// Obtén referencias a los botones
-var boton1 = document.getElementById("bHome");
-var boton2 = document.getElementById("bSonreMi");
-var boton3 = document.getElementById("bTecnologias");
-var boton4 = document.getElementById("bProyectos");
-var boton5 = document.getElementById("bContacto");
-var boton6 = document.getElementById("bSobreMi2");
-var boton7 = document.getElementById("bCalculadora");
-var boton8 = document.getElementById("bFacturador");
-var boton9 = document.getElementById("bAppChat");
-
-
-// Redirecciones internas (GitHub Pages)
-boton1.addEventListener("click", function () {
+boton1.addEventListener("click", () => {
     window.location.href = "index.html";
 });
 
-boton2.addEventListener("click", function () {
+boton2.addEventListener("click", () => {
     window.location.href = "sobreMi.html";
 });
 
-boton3.addEventListener("click", function () {
+boton3.addEventListener("click", () => {
     window.location.href = "Habilidades.html";
 });
 
-boton4.addEventListener("click", function () {
+boton4.addEventListener("click", () => {
     window.location.href = "Proyectos.html";
 });
 
-boton5.addEventListener("click", function () {
+boton5.addEventListener("click", () => {
     window.location.href = "Contacto.html";
 });
 
-boton6.addEventListener("click", function () {
+boton6.addEventListener("click", () => {
     window.location.href = "sobreMi.html";
 });
 
-// Redirecciones externas (están bien así)
-boton7.addEventListener("click", function () {
+boton7.addEventListener("click", () => {
     window.location.href = "https://github.com/Eliane97/Android-Calculadora.git";
 });
 
-boton8.addEventListener("click", function () {
+boton8.addEventListener("click", () => {
     window.location.href = "https://github.com/Eliane97/Facturador.git";
 });
 
-boton9.addEventListener("click", function () {
+boton9.addEventListener("click", () => {
     window.location.href = "https://github.com/PolNun/lpet_android.git";
 });

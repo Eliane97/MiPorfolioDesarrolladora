89fbf624214d179d8ba1b29b5f7cfc80b3aee447
Eliane <101228113+Eliane97@users.noreply.github.com>
2025-12-09 21:33:33 -0300

    Update accion.js
    mmm

--- accion.js
+++ accion.js
@@ -1,35 +1,27 @@
-boton1.addEventListener("click", () => {
-    window.location.href = "index.html";
-});
+// Botones
+var boton1 = document.getElementById("bHome");
+var boton2 = document.getElementById("bSonreMi");
+var boton3 = document.getElementById("bTecnologias");
+var boton4 = document.getElementById("bProyectos");
+var boton5 = document.getElementById("bContacto");
+var boton6 = document.getElementById("bSobreMi2");
 
-boton2.addEventListener("click", () => {
-    window.location.href = "sobreMi.html";
-});
+// ⚠️ IMPORTANTE: cambia ESTA parte por el nombre EXACTO de tu repositorio de GitHub Pages
+var baseURL = "https://eliane97.github.io/TU_REPOSITORIO/";
 
-boton3.addEventListener("click", () => {
-    window.location.href = "Habilidades.html";
-});
+// Redirecciones internas (páginas del portfolio)
+boton1.addEventListener("click", () => window.location.href = baseURL + "index.html");
+boton2.addEventListener("click", () => window.location.href = baseURL + "sobreMi.html");
+boton3.addEventListener("click", () => window.location.href = baseURL + "Habilidades.html");
+boton4.addEventListener("click", () => window.location.href = baseURL + "Proyectos.html");
+boton5.addEventListener("click", () => window.location.href = baseURL + "Contacto.html");
+boton6.addEventListener("click", () => window.location.href = baseURL + "sobreMi.html");
 
-boton4.addEventListener("click", () => {
-    window.location.href = "Proyectos.html";
-});
+// Si más adelante querés agregar proyectos externos, podés poner links normales:
+var boton7 = document.getElementById("bCalculadora");
+var boton8 = document.getElementById("bFacturador");
+var boton9 = document.getElementById("bAppChat");
 
-boton5.addEventListener("click", () => {
-    window.location.href = "Contacto.html";
-});
-
-boton6.addEventListener("click", () => {
-    window.location.href = "sobreMi.html";
-});
-
-boton7.addEventListener("click", () => {
-    window.location.href = "https://github.com/Eliane97/Android-Calculadora.git";
-});
-
-boton8.addEventListener("click", () => {
-    window.location.href = "https://github.com/Eliane97/Facturador.git";
-});
-
-boton9.addEventListener("click", () => {
-    window.location.href = "https://github.com/PolNun/lpet_android.git";
-});
+if (boton7) boton7.addEventListener("click", () => window.location.href = "https://github.com/Eliane97/Android-Calculadora.git");
+if (boton8) boton8.addEventListener("click", () => window.location.href = "https://github.com/Eliane97/Facturador.git");
+if (boton9) boton9.addEventListener("click", () => window.location.href = "https://github.com/PolNun/lpet_android.git");
